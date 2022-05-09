ba7d25a7f5a3bf2f1439cb2cc12ccc5d6b55a418
Alan Staves <alan.staves@microfocus.com>
2022-05-09 14:48:01 +0100

    Force user to create importscript by using different template filename

--- tutorial/importsetup.sh
+++ tutorial/importsetup.sh
@@ -9,14 +9,22 @@ fi
 if [ ! -d "$FILEPATH" ]; then
     echo "WARNING: Path does not exist."
 fi
-WRITEBAK="y"
-if [ -f "$SCRIPTDIR/BANKDEMO.xml.bak" ]; then
-    echo "$SCRIPTDIR/BANKDEMO.xml.bak already exists. Do you want to override? (y/n) "
-    read WRITEBAK
+WRITEXML="y"
+if [ -f "$SCRIPTDIR/BANKDEMO.xml" ]; then
+    if [ -f "$SCRIPTDIR/BANKDEMO.xml.bak" ]; then
+        echo "$SCRIPTDIR/BANKDEMO.xml.bak already exists. Do you want to override? (y/n) "
+        read WRITEXML
+        if [ "$WRITEXML" = "y" -o "$WRITEXML" = "Y" ]; then
+            cp "$SCRIPTDIR/BANKDEMO.xml" "$SCRIPTDIR/BANKDEMO.xml.bak"
+        fi
+    fi
+
 fi
-if [ "$WRITEBAK" = "y" -o "$WRITEBAK" = "Y" ]; then
-    cp "$SCRIPTDIR/BANKDEMO.xml" "$SCRIPTDIR/BANKDEMO.xml.bak"
+if [ "$WRITEXML" = "y" -o "$WRITEXML" = "Y" ]; then
+    cp "$SCRIPTDIR/BANKDEMO.template" "$SCRIPTDIR/BANKDEMO.xml"
+    sed -i -e "s!__IMPORT_FILE_DIR__!$FILEPATH!" "$SCRIPTDIR/BANKDEMO.xml" 
+    sed -i -e "s!PS=;!PS=:!" "$SCRIPTDIR/BANKDEMO.xml"
+    sed -i -e "s!OS=Windows!OS=Linux!" "$SCRIPTDIR/BANKDEMO.xml"
+else
+    echo "Error: Enterprise Server definition was not created."
 fi
-sed -i -e "s!__IMPORT_FILE_DIR__!$FILEPATH!" "$SCRIPTDIR/BANKDEMO.xml" 
-sed -i -e "s!PS=;!PS=:!" "$SCRIPTDIR/BANKDEMO.xml"
-sed -i -e "s!OS=Windows!OS=Linux!" "$SCRIPTDIR/BANKDEMO.xml"
