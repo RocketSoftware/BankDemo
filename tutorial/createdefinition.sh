eb93240b707d94920449ac8ecbd159474b9803e5
Alan Staves <alan.staves@microfocus.com>
2022-05-17 14:50:31 +0100

    Rename scripts to better represent what they do (and include copyright)

--- tutorial/createdefinition.sh
+++ tutorial/createdefinition.sh
@@ -1,4 +1,22 @@
 #! /bin/sh
+
+###############################################################
+# Copyright (C) 2022 Micro Focus.  All Rights Reserved
+# This software may be used, modified, and distributed
+# (provided this notice is included without modification)
+# solely for internal demonstration purposes with other
+# Micro Focus software, and is otherwise subject to the EULA at
+# https://www.microfocus.com/en-us/legal/software-licensing.
+#
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ALL IMPLIED
+# WARRANTIES, INCLUDING THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE,
+# SHALL NOT APPLY.
+# #TO THE EXTENT PERMITTED BY LAW, IN NO EVENT WILL
+# MICRO FOCUS HAVE ANY LIABILITY WHATSOEVER IN CONNECTION
+# WITH THIS SOFTWARE.
+###############################################################
+
 SCRIPTDIR="$(dirname "$(readlink -f "$0")")"
 if [ $# = 0 ]; then
     FILEPATH="$SCRIPTDIR"
